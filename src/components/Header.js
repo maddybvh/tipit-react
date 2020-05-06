72d5238dbffe8e9566760bf77cdaf0d2e468554f
maddybvh <maddybvh@gmail.com>
2020-05-06 12:16:41 -0400

    Add react-modal component and do initial set up

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-//import InfoModal from './InfoModal';
+import InfoModal from './InfoModal';
 //import SettingsModal from './SettingsModal';
 import { Title } from './Title';
 import { Dashes } from './Dashes';
@@ -7,7 +7,7 @@ import { Dashes } from './Dashes';
 export const Header = () => (
   <React.Fragment>
     <React.Fragment>
-      {/* <InfoModal /> */}
+      <InfoModal />
       <Title /> 
       {/* <SettingsModal /> */}
     </React.Fragment>
--- src/components/InfoModal.js
+++ src/components/InfoModal.js
@@ -1,97 +1,107 @@
 import React, { useContext } from 'react';
-import { Modal, Text, View, StyleSheet, Image, Linking, TouchableOpacity, SafeAreaView } from 'react-native';
 import { Title } from './Title';
 import { Footer } from './Footer';
 import { Dashes } from './Dashes';
 import useModal from '../hooks/useModal.js'
 import AppContext from './AppContext';
+import styles from '../theme/appstyles.module.css';
+import Modal from 'react-modal';
 
 export const InfoModal = () => {
    const context = useContext(AppContext)
    const theme = context.theme
    const { colors } = context.useTheme()
-   const { isShowing, toggle } = useModal();
+   //const { isShowing, toggle } = useModal();
+
+   const [modalIsOpen,setIsOpen] = React.useState(false);
+   function openModal() {
+     setIsOpen(true);
+   }
+  
+   function afterOpenModal() {
+     // references are now sync'd and can be accessed.
+   }
+  
+   function closeModal(){
+     setIsOpen(false);
+   }
 
    return (
-      <React.Fragment>
+      <div>
          <Modal 
-            animationType = {"slide"} transparent = {false}
-            visible = {isShowing}
-            onRequestClose = {() => { console.log("Modal has been closed.")}}
+            isOpen = {modalIsOpen}
+            onAfterOpen = {afterOpenModal}
+            onRequestClose = {closeModal}
             >
-            <View style={{flex: 1, backgroundColor: colors.background}}>
-               <SafeAreaView style = {[styles.modal, {backgroundColor: colors.background}]}>
-                  <View style={{alignSelf: 'flex-start', flexDirection: 'row', justifyContent: 'space-between', alignContent: 'center'}}>
-                     <TouchableOpacity style={{padding: 5, flex: 1}} onPress = {() => {toggle()}}> 
-                           <Text style = {[styles.clear, {color: colors.clear}]}>X</Text>
-                     </TouchableOpacity>
-                        <Title style = {{flex: 1}}/>
-                     <View style = {{flex: 1}}></View>
-                  </View>
+            <div>
+               <div className={styles.modal}>
+                  <div style={{alignSelf: 'flex-start', flexDirection: 'row', justifyContent: 'space-between', alignContent: 'center'}}>
+                     <button style={{padding: 5, flex: 1}} onClick = {closeModal}> 
+                           X
+                     </button>
+                     <Title />
+                  </div>
                   <Dashes />
-                  <Text style = {[styles.normalText, {fontFamily: 'JetBrainsMono-Bold', color: colors.text}]}>What is this app?</Text>
-                  <Text style = {[styles.description, {color: colors.text}]}>A means to enhance palindromic whimsy.</Text>
-                  <View style={{flex: 2, justifyContent: 'flex-end', marginBottom: 30}}>
-                     <View style={{flexDirection: 'row', justifyContent: 'space-between', alignContent: 'center'}}>
-                        <Text style = {[styles.normalText, {textAlign: 'left', color: colors.text, flex: 9}]}>(c) Savas Labs 2020</Text>
-                        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center', alignSelf: 'flex-end'}}>
-                           <Image source={require('../../assets/ava-blue.png')} />
-                        </View>
+                  <div className={styles.normalText}>What is this app?</div>
+                  <div className={styles.normalText}>A means to enhance palindromic whimsy.</div>
+                  <div style={{display: 'flex', justifyContent: 'flex-end', marginBottom: 30}}>
+                     <div style={{flexDirection: 'row', justifyContent: 'space-between', alignContent: 'center'}}>
+                        <div style = {{textAlign: 'left'}}>(c) Savas Labs 2020</div>
+                        <div style={{flex: 1, justifyContent: 'center', alignItems: 'center', alignSelf: 'flex-end'}}>
+                           <img src={require('../assets/ava-blue.png')} />
+                        </div>
                         
-                     </View>
-                     <View style={{flexDirection: 'row', justifyContent: 'space-between', alignContent: 'center'}}>
-                        <Text style = {[styles.normalText, {color: colors.link, textDecorationLine: 'underline', textAlign: 'left', flex: 9}]}
-                           onPress={ ()=> Linking.openURL('https:savaslabs.com') }>
+                     </div>
+                     <div style={{flexDirection: 'row', justifyContent: 'space-between', alignContent: 'center'}}>
+                        <div>
                            Learn more about our #labs initiative.
-                        </Text>
-                        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center', alignSelf: 'flex-end'}}>
-                           <Image source={require('../../assets/labs.png')}/>
-                        </View>
-                     </View>
-                  </View>
+                        </div>
+                        <div>
+                           <img src={require('../assets/labs.png')}/>
+                        </div>
+                     </div>
+                  </div>
                   <Footer />
-               </SafeAreaView>
-            </View>  
+               </div>
+            </div>  
          </Modal>
-         <TouchableOpacity style={{padding: 5}} onPress = {() => {toggle()}}>
-            <Image 
-               source={
-                  (theme == 'light') ? require('../../assets/info.png') : require('../../assets/info-white.png')
-               }
+         <button style={{padding: 5}} onClick = {openModal}>
+            <img 
+               src={(theme == 'light') ? require('../assets/info.png') : require('../assets/info-white.png')}
                />
-            </TouchableOpacity>
-      </View>
+            </button>
+      </div>
 )}
 
 export default InfoModal
 
-const styles = StyleSheet.create ({
-    container: {
-       padding: 10,
-    },
-    modal: {
-      flex: 1,
-      marginHorizontal: 8,
-    },
-    normalText: {
-      fontFamily: 'JetBrainsMono-Regular', 
-      fontSize: 14,
-      lineHeight: 20,
-      marginTop: 10,
-      textAlign: 'center',
-    },
-    description: {
-      fontFamily: 'JetBrainsMono-Regular', 
-      fontSize: 20,
-      lineHeight: 28,
-      marginTop: 10,
-      textAlign: 'center',
-      padding: 20,
-    },
-    clear: {
-       fontFamily: 'JetBrainsMono-Bold',
-       padding: 10,
-       fontSize: 18,
-    }
-})
+// const styles = StyleSheet.create ({
+//     container: {
+//        padding: 10,
+//     },
+//     modal: {
+//       flex: 1,
+//       marginHorizontal: 8,
+//     },
+//     normalText: {
+//       fontFamily: 'JetBrainsMono-Regular', 
+//       fontSize: 14,
+//       lineHeight: 20,
+//       marginTop: 10,
+//       textAlign: 'center',
+//     },
+//     description: {
+//       fontFamily: 'JetBrainsMono-Regular', 
+//       fontSize: 20,
+//       lineHeight: 28,
+//       marginTop: 10,
+//       textAlign: 'center',
+//       padding: 20,
+//     },
+//     clear: {
+//        fontFamily: 'JetBrainsMono-Bold',
+//        padding: 10,
+//        fontSize: 18,
+//     }
+// })
 
