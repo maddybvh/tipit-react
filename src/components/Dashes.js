b8e6802e9df328c931bd8a117f60801709555781
maddybvh <maddybvh@gmail.com>
2020-05-06 11:30:17 -0400

    Add svg version of dashes

--- src/components/Dashes.js
+++ src/components/Dashes.js
@@ -6,10 +6,11 @@ export const Dashes = () => {
   const theme = useContext(AppContext).theme;
   return (
     <img 
-      source={
-         (theme == 'light') ? require('../assets/dashes.png') : require('../assets/dashes-white.png')
-      }
-      style={{marginTop: 10, marginBottom: 10, width: '100%'}} />
+      src={require('../assets/dashes.svg')}
+        //  (theme == 'light') ? require('../assets/dashes.png') : require('../assets/dashes-white.png')
+      alt='dashes'
+      //style={{marginTop: 10, marginBottom: 10, width: '100%'}} 
+      style={{width: '100%'}}
+      ></img>
   )
 }
-
