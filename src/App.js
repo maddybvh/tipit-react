c0a275bc22fa5b6c6cf40876ab6c8046a71d6b3a
maddybvh <maddybvh@gmail.com>
2020-05-08 13:27:01 -0400

    WIP

--- src/App.js
+++ src/App.js
@@ -7,46 +7,38 @@ import AppContext, { useTheme } from './components/AppContext';
 import { themedColors } from './theme/index'
 import styles from './theme/appstyles.module.css';
 
-
-
 export default function App () {
-  // const _storeData = async () => {
-  //   try {
-  //     await AsyncStorage.removeItem('@store:appContext')
-  //     settingsToSave = {
-  //       defaultTipLow: userSettings.defaultTipLow,
-  //       defaultTipHigh: userSettings.defaultTipHigh,
-  //       theme: userSettings.theme,
-  //     }
-  //     await AsyncStorage.setItem('@store:appContext', JSON.stringify(settingsToSave));
-  //   } catch (error) {
-  //     console.log('Error saving user settings.')
-  //   }
-  // };
+  const _storeData = () => {
+    try {
+      localStorage.clear();
+      const settingsToSave = {
+        defaultTipLow: userSettings.defaultTipLow,
+        defaultTipHigh: userSettings.defaultTipHigh,
+        theme: userSettings.theme,
+      }
+      localStorage.setItem('@store:appContext', settingsToSave);
+    } catch (error) {
+      console.log('Error saving user settings.')
+    }
+  };
   
-  // const [initialAppLoad, setInitialAppLoad] = useState(true);
+  const [initialAppLoad, setInitialAppLoad] = useState(true);
 
-  // const _retrieveData = async () => {
-  //   try {
-  //     const value = await AsyncStorage.getItem('@store:appContext');
-  //     const savedSettings = JSON.parse(value)
-      
-  //     if (savedSettings !== null) {
-  //       setDefaultTipLow(savedSettings.defaultTipLow)
-  //       setDefaultTipHigh(savedSettings.defaultTipHigh)
-  //       setTheme(savedSettings.theme)
-  //     }
-  //   } catch (error) {
-  //     console.log('No user setting data was retrieved');
-  //   }
-  //   setInitialAppLoad(false);
-  // };
-  
-  // let [fontsLoaded] = useFonts({
-  //   'JetBrainsMono-Regular': require('./assets/fonts/JetBrainsMono-Regular.ttf'),
-  //   'JetBrainsMono-Bold': require('./assets/fonts/JetBrainsMono-Bold.ttf'),
-  //   'JetBrainsMono-Italic': require('./assets/fonts/JetBrainsMono-Italic.ttf'),
-  // });
+  const _retrieveData = () => {
+    try {
+      const value = localStorage.getItem('@store:appContext');
+      const savedSettings = value
+      if (savedSettings !== null) {
+        setDefaultTipLow(savedSettings.defaultTipLow)
+        setDefaultTipHigh(savedSettings.defaultTipHigh)
+        setTheme(savedSettings.theme)
+      }
+      console.log(savedSettings)
+    } catch (error) {
+      console.log('No user setting data was retrieved');
+    }
+    setInitialAppLoad(false);
+  };
 
   const [defaultTipLow, setDefaultTipLow] = useState('18');
   const [defaultTipHigh, setDefaultTipHigh] = useState('25');
@@ -77,15 +69,7 @@ export default function App () {
   //This is a repeat of useTheme() in /AppContext
   const colors = userSettings.theme ? themedColors[userSettings.theme] : themedColors.default
 
-  //useEffect(() => { initialAppLoad ? _retrieveData() : _storeData() })
-
-  // if (!fontsLoaded) {
-  //   return <React.Fragment>
-  //     App Loading
-  //   </React.Fragment>;
-  // }
-  // else {
-
+  useEffect(() => { initialAppLoad ? _retrieveData() : _storeData() })
 
     return (
       <AppContext.Provider value={userSettings}>
