29aa247c1020349cc10a86832fd21b9743b43d6e
maddybvh <maddybvh@gmail.com>
2020-07-16 09:10:07 -0400

    Remove unused App.css file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
-import { Header } from './components/Header'
-import { Calculator } from './components/Calculator'
-import { Footer } from './components/Footer'
-import AppContext, { useTheme } from './components/AppContext';
-import { themedColors } from './theme/index'
-import styles from './theme/appstyles.module.css';
+import React, { useState, useEffect } from "react";
+import { Header } from "./components/Header";
+import { Calculator } from "./components/Calculator";
+import { Footer } from "./components/Footer";
+import AppContext, { useTheme } from "./components/AppContext";
+import { themedColors } from "./theme/index";
+import styles from "./theme/appstyles.module.css";
 
-export default function App () {
+export default function App() {
   const _storeData = () => {
     try {
       localStorage.clear();
@@ -15,71 +14,77 @@ export default function App () {
         defaultTipLow: userSettings.defaultTipLow,
         defaultTipHigh: userSettings.defaultTipHigh,
         theme: userSettings.theme,
-      }
-      localStorage.setItem('@store:appContext', JSON.stringify(settingsToSave));
+      };
+      localStorage.setItem("@store:appContext", JSON.stringify(settingsToSave));
     } catch (error) {
-      console.log('Error saving user settings.')
+      console.log("Error saving user settings.");
     }
   };
-  
+
   const [initialAppLoad, setInitialAppLoad] = useState(true);
 
   const _retrieveData = () => {
     try {
-      const value = localStorage.getItem('@store:appContext');
-      const savedSettings = JSON.parse(value)
+      const value = localStorage.getItem("@store:appContext");
+      const savedSettings = JSON.parse(value);
       if (savedSettings !== null) {
-        setDefaultTipLow(savedSettings.defaultTipLow)
-        setDefaultTipHigh(savedSettings.defaultTipHigh)
-        setTheme(savedSettings.theme)
+        setDefaultTipLow(savedSettings.defaultTipLow);
+        setDefaultTipHigh(savedSettings.defaultTipHigh);
+        setTheme(savedSettings.theme);
       }
     } catch (error) {
-      console.log('No user setting data was retrieved');
+      console.log("No user setting data was retrieved");
     }
     setInitialAppLoad(false);
   };
 
-  const [defaultTipLow, setDefaultTipLow] = useState('18');
-  const [defaultTipHigh, setDefaultTipHigh] = useState('25');
-  const [theme, setTheme] = useState('light');
+  const [defaultTipLow, setDefaultTipLow] = useState("18");
+  const [defaultTipHigh, setDefaultTipHigh] = useState("25");
+  const [theme, setTheme] = useState("light");
 
-  const updateTipLowContext = (input) => {
-    setDefaultTipLow(input);   
-  }
+  const updateTipLowContext = input => {
+    setDefaultTipLow(input);
+  };
 
-  const updateTipHighContext = (input) => {
+  const updateTipHighContext = input => {
     setDefaultTipHigh(input);
-  }
+  };
 
   const toggleTheme = () => {
-    theme === 'light' ? setTheme('dark') : setTheme('light');
-  }
+    theme === "light" ? setTheme("dark") : setTheme("light");
+  };
 
-  const userSettings = { 
-    defaultTipLow: defaultTipLow, 
-    defaultTipHigh: defaultTipHigh, 
-    theme: theme, 
-    useTheme, 
-    toggleTheme, 
-    updateTipLowContext, 
-    updateTipHighContext, 
-  }
-  
+  const userSettings = {
+    defaultTipLow: defaultTipLow,
+    defaultTipHigh: defaultTipHigh,
+    theme: theme,
+    useTheme,
+    toggleTheme,
+    updateTipLowContext,
+    updateTipHighContext,
+  };
+
   //This is a repeat of useTheme() in /AppContext
-  const colors = userSettings.theme ? themedColors[userSettings.theme] : themedColors.default
+  const colors = userSettings.theme
+    ? themedColors[userSettings.theme]
+    : themedColors.default;
 
-  useEffect(() => { initialAppLoad ? _retrieveData() : _storeData() })
+  useEffect(() => {
+    initialAppLoad ? _retrieveData() : _storeData();
+  });
 
-    return (
-      <AppContext.Provider value={userSettings}>
-        <div className={styles.body} style={{backgroundColor: colors.background, color: colors.text}}>
-          <div className={styles.container}>
-            <Header />
-            <Calculator />
-            <Footer />
-          </div>
+  return (
+    <AppContext.Provider value={userSettings}>
+      <div
+        className={styles.body}
+        style={{ backgroundColor: colors.background, color: colors.text }}
+      >
+        <div className={styles.container}>
+          <Header />
+          <Calculator />
+          <Footer />
         </div>
-      </AppContext.Provider>
-    )
+      </div>
+    </AppContext.Provider>
+  );
 }
-
